442d70e9899da39c8ab466fa80665fdc770dcd54
Yoshio Miyamae <yoshio.miyamae@famoce-succellion.com>
2017-10-16 23:38:54 +0000

    importできない問題を修正

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import 'bulma/css/bulma.css';
-import * as Columns from './columns';
-import * as Elements from './elements';
-import * as Form from './form';
-import * as Layout from './layout';
-import * as Modifiers from './modifiers';
-import * as Components from './components';
-
-export default {...Columns, ...Elements, ...Form, ...Layout, ...Modifiers, ...Components};
+export * from './columns';
+export * from './elements';
+export * from './form';
+export * from './layout';
+export * from './modifiers';
+export * from './components';
