b25fb2452736c3a2ae1538241e377dd9ee455d9b
Yoshio Miyamae <yoshio.miyamae@famoce-succellion.com>
2017-10-16 23:40:38 +0000

    importできない問題を修正

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 require("bulma/css/bulma.css");
-const Columns = require("./columns");
-const Elements = require("./elements");
-const Form = require("./form");
-const Layout = require("./layout");
-const Modifiers = require("./modifiers");
-const Components = require("./components");
-exports.default = Object.assign({}, Columns, Elements, Form, Layout, Modifiers, Components);
+__export(require("./columns"));
+__export(require("./elements"));
+__export(require("./form"));
+__export(require("./layout"));
+__export(require("./modifiers"));
+__export(require("./components"));
 //# sourceMappingURL=main.js.map
