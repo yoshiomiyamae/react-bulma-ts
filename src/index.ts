1092a104b6aa3f99a0ac0b77a082b8fbe4b1347b
Yoshio Miyamae <yoshio.miyamae@famoce-succellion.com>
2019-08-10 08:53:12 +0900

    remove all

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import 'bulma/css/bulma.css';
-export * from './columns';
-export * from './elements';
-export * from './form';
-export * from './layout';
-export * from './modifiers';
-export * from './components';
--- src/layout/index.ts
+++
@@ -1,8 +0,0 @@
-import {Container} from './container';
-import {Footer} from './footer';
-import {Hero, HeroBody, HeroFoot, HeroHead} from './hero';
-import {Level, LevelItem, LevelLeft, LevelRight} from './level';
-import {Media, MediaContent, MediaItem, MediaLeft, MediaRight} from './media-object';
-import {Section} from './section';
-
-export {Container, Footer, Hero, HeroBody, HeroFoot, HeroHead, Level, LevelItem, LevelLeft, LevelRight, Media, MediaContent, MediaItem, MediaLeft, MediaRight, Section};
