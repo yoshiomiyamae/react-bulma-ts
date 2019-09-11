c6602045a648893f4af9da219a2ca690dd65d4e8
yoshiomiyamae <yoshio.miyamae@famoce-succellion.com>
2019-09-11 10:23:53 +0900

    image, notification, progressbar, tableの実装

---
+++ src/element/image/image-size.ts
@@ -0,0 +1,28 @@
+export enum ImageSize {
+    Sixteen = 'is-16x16',
+    TwenteeFour = 'is-24x24',
+    ThirtyTwo = 'is-32x32',
+    FortyEight = 'is-48x48',
+    SixtyFour = 'is-64x64',
+    NintySix = 'is-96x96',
+    OneHandletEight = 'is-128x128',
+}
+
+export enum ImageOption {
+    Square = 'is-square',
+    OneByOne = 'is-1by1',
+    FiveByFour = 'is-5by4',
+    FourByThree = 'is-4by3',
+    ThreeByTwo = 'is-3by2',
+    FiveByThree = 'is-5by3',
+    SixteenByNine = 'is-16by9',
+    TwoByOne = 'is-2by1',
+    ThreeByOne = 'is-3by1',
+    FourByFive = 'is-4by5',
+    ThreeByFour = 'is-3by4',
+    TwoByThree = 'is-2by3',
+    ThreeByFive = 'is-3by5',
+    NineBySixteen = 'is-9by16',
+    OneByTwo = 'is-1by2',
+    OneByThree = 'is-1by3',
+}
