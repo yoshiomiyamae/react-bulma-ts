df1bad967dde8c846517e9b00c5556f75ef3cb32
Yoshio Miyamae <yoshio.miyamae@famoce-succellion.com>
2020-11-17 23:02:15 +0900

    bug fix

--- src/element/tag/tags.tsx
+++ src/element/tag/tags.tsx
@@ -3,12 +3,12 @@ import { classNameBuilder, BaseComponent, BaseProps} from '../../common';
 import { TileChild } from '../../layout/tile/tile-child';
 
 interface TagsProps extends BaseProps<HTMLDivElement> {
-  hasAddon?: boolean;
+  hasAddons?: boolean;
 }
 
 export class Tags extends BaseComponent<TagsProps> {
   render () {
-    return <div className={classNameBuilder(['tags', (this.props.isTileChild ? TileChild.TileChild : null), (this.props.hasAddon ? 'has-addons' : null)])} {...this.props}>
+    return <div className={classNameBuilder(['tags', (this.props.isTileChild ? TileChild.TileChild : null), (this.props.hasAddons ? 'has-addons' : null)])} {...this.props}>
         {this.props.children}
     </div>
   }
