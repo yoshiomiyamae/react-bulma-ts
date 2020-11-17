ba1f85240a80f3568bff67e5e7cc687f56ce116b
Yoshio Miyamae <yoshio.miyamae@famoce-succellion.com>
2020-11-17 22:47:17 +0900

    add GroupedMultiline to FieldOption

--- src/form/field/field-option.ts
+++ src/form/field/field-option.ts
@@ -2,6 +2,7 @@ export enum FieldOption {
   Grouped = 'is-grouped',
   GroupedCentered = 'is-grouped-centered',
   GroupedRight = 'is-grouped-right',
+  GroupedMultiline = 'is-grouped-multiline',
   HasAddons = 'has-addons',
   HasAddonsCentered = 'has-addons-centered',
   HasAddonsRight = 'has-addons-right',
